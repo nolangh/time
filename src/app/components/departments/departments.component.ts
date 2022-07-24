45df8fadaeb17f558b2c6495879d45db97f7011b
nolangh <nolanghiggins@gmail.com>
2022-07-24 17:28:22 -0600

    Finished unit-5

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrls: ['./departments.component.scss']
+  styleUrls: ['./departments.component.scss'],
 })
 export class DepartmentsComponent implements OnInit {
-
-  constructor() { }
+  departments: Department[];
+  constructor(private departmentsService: DepartmentsService) {}
 
   ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
   }
-
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
-  exports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
