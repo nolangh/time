706f3aa41fb2dcdbb6dda0716958af3da2119ead
nolangh <nolanghiggins@gmail.com>
2022-07-26 12:41:33 -0600

    -a

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -7,7 +9,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./timesheet.component.scss'],
 })
 export class TimesheetComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  departments: Department[] | undefined;
+  department: Department | undefined;
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService
+  ) {}
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(
+      (department) => department.id === this.route.snapshot.params['id']
+    );
+  }
 }
