e9d81e2f31ef0f02ca29c34b253fc79d08f4378b
nolangh <nolanghiggins@gmail.com>
2022-10-23 18:46:46 -0600

    Finished 12

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
 
@@ -10,6 +11,7 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[];
+  $departments: Observable<Department[]> | undefined;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,10 +19,10 @@ export class DepartmentsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.departments = this.departmentsService.departments;
-    this.departmentsService.getDepartments().subscribe((departments) => {
-      this.departments = departments;
-    });
+    // this.departmentsService.getDepartments().subscribe((departments) => {
+    //  this.departments = departments;
+    // });
+    this.$departments = this.departmentsService.getDepartments();
   }
 
   goToDepartment(departmentId: string): void {
