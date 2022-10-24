fdf9b7156de69016cf377450836ae54fcc589fae
nolangh <nolanghiggins@gmail.com>
2022-10-23 19:19:53 -0600

    Never touching angular again

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -5,7 +5,7 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 import { AbstractControl, FormControl, ValidatorFn } from '@angular/forms';
 import { Employee } from 'src/app/interfaces/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-timesheet',
@@ -38,11 +38,21 @@ export class TimesheetComponent implements OnInit {
   ngOnInit(): void {
     this.$departments = this.departmentsService.getDepartments();
 
-    this.$departments.subscribe((x) => {
-      this.department = x.find(
-        (dept) => dept.id === this.route.snapshot.params['id']
-      );
-    });
+    this.$departments
+      .pipe(
+        switchMap((departments) => {
+          this.department = departments.find(
+            (dept) => dept.id === this.route.snapshot.params['id']
+          );
+          return this.employeeService.getEmployeeHoursByDepartment(
+            this.department.id
+          );
+        }),
+        tap((employees) => {
+          this.employees = employees;
+        })
+      )
+      .subscribe();
   }
 
   addEmployee(): void {
@@ -93,13 +103,21 @@ export class TimesheetComponent implements OnInit {
     );
   }
 
-  deleteEmployee(index: number): void {
+  deleteEmployee(employee: Employee, index: number): void {
+    if (employee.id) {
+      this.employeeService.deleteEmployeeHours(employee);
+    }
+
     this.employees.splice(index, 1);
   }
 
   submit(): void {
     this.employees.forEach((employee) => {
-      this.employeeService.saveEmployeeHours(employee);
+      if (employee.id) {
+        this.employeeService.updateEmployeeHours(employee);
+      } else {
+        this.employeeService.saveEmployeeHours(employee);
+      }
     });
 
     this.router.navigate(['./departments']);
