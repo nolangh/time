39438b7dbade7372fb2b8587774d4eda8cd41ee3
nolangh <nolanghiggins@gmail.com>
2022-10-23 18:32:49 -0600

    finished 9

--- src/app/components/analytics-table/analytics-table.component.ts
+++ src/app/components/analytics-table/analytics-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Employee } from 'src/app/interfaces/employee';
 
 @Component({
@@ -7,6 +7,8 @@ import { Employee } from 'src/app/interfaces/employee';
   styleUrls: ['./analytics-table.component.scss'],
 })
 export class AnalyticsTableComponent implements OnInit {
+  @Input()
+  departmentId: string | undefined;
   weekdays: string[] = [
     'monday',
     'tuesday',
@@ -73,6 +75,21 @@ export class AnalyticsTableComponent implements OnInit {
   ];
 
   constructor() {}
+  ngOnInit(): void {
+    this.employees = this.employeeData.filter(
+      (employee) => employee.departmentId === this.departmentId
+    );
+  }
 
-  ngOnInit(): void {}
+  getTotalHours(employee: Employee): number {
+    return (
+      employee.monday +
+      employee.tuesday +
+      employee.wednesday +
+      employee.thursday +
+      employee.friday +
+      employee.saturday +
+      employee.sunday
+    );
+  }
 }
